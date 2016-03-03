b515029926a911cff3ee43ddfc4c67b5478b9b32
Adel Lahlou <adellahlou@users.noreply.github.com>
2016-03-03 10:46:20 -0600

    Fixed message type. Added member accessors

--- src/chat/message.rs
+++ src/chat/message.rs
@@ -1,20 +1,20 @@
 use super::types::*;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum MessageType {
 	Join,
 	Chat,
 	Leave,
-    /* 2/24/16 james: i don't know what this is
-               let me know what this is about later.
-               im hacking through to compile this so commenting out for now
-	Confirm(MessageType),
-               */
-
+	Action ,
+	Confirm(Id),
+	//used to confirm Message actions
 }
 
+/// Uniquely Identifiable Message Struct via message_id, date, sender
+/// Gurantee is that the same user will not generate the same id at the same time
 #[derive(Debug, PartialEq)]
 pub struct Message  {
+	message_id : Id,
 	date : Time,
 	sender : Id,
 	receiver : Id,
@@ -23,13 +23,38 @@ pub struct Message  {
 }
 
 impl Message {
-	pub fn new(date : Time, sender : Id, receiver : Id, message_type : MessageType, message : String) -> Message {
+	pub fn new(message_id : Id, date : Time, sender : Id, receiver : Id, message_type : MessageType, message : String) -> Message {
    	Message {
+		message_id : message_id,
    		date : date,
    		sender : sender,
    		receiver : receiver,
    		message_type : message_type,
    		message : message
    	}
+   }
+
+   fn id(&self) -> &Id {
+	   &self.message_id
+   }
+
+   fn date(&self) -> Time {
+	  self.date.clone()
+   }
+
+   fn sender(&self) -> &Id {
+	   &self.sender
+   }
+
+   fn receiver(&self) -> &Id {
+	   &self.receiver
+   }
+
+   fn message_type(&self) -> MessageType {
+	   self.message_type.clone()
+   }
+
+   fn message(&self) -> &String {
+	   &self.message
    }
 }
