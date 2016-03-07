f2fb0566109477e00a13f3c8cd6f0e4715b5ec9a
Adel Lahlou <adellahlou@users.noreply.github.com>
2016-03-07 01:06:22 -0600

    Refactored chat app into functions and prettiness

--- src/chat/chat_app.rs
+++ src/chat/chat_app.rs
@@ -41,95 +41,13 @@ impl<T : Write> ChatApp<T> {
     pub fn handle_server_message(&mut self, tok : Token, s : String) {
         if let Some(cm) = Message::from_string(s) {
             match cm.message_type() {
-                Connect => {
-                    let player_id = cm.message().clone();
-                    let mid = cm.id().clone();
+                Connect => { self.handle_connect(cm, tok); },
 
-                    if let Vacant(ic_entry) = self.id_to_conn.entry(player_id.clone()) {
-                        //new connection - new name
-                        if let Vacant(ci_entry) = self.conn_to_id.entry(tok) {
-                            ic_entry.insert(tok);
-                            ci_entry.insert(player_id);
+            	Join =>   { self.handle_join(cm, tok); }
 
-                            let mconfirm = Message::new(cm.id().clone(), UTC::now(),
-                                        "SERVER".to_string(), cm.sender().clone(), Confirm(mid), "".to_string());
-                            self.handle_to_mio_to_write_with.write(mconfirm.into_bytes().as_slice());
-                        }
+            	Leave =>  { self.handle_leave(cm, tok); },
 
-                        //old connection wants new name
-                        else {
-                            //could handle this more elegantly, but very unlikely unless spoofing packets
-                            unreachable!();
-                        }
-                    }
-
-                    // id already taken, refuse "connection"
-                    else {
-                        let mreject = Message::new(cm.id().clone(), UTC::now(),
-                                    "SERVER".to_string(), cm.sender().clone(), Reject(mid), "Requested ID is already taken".to_string());
-                        self.handle_to_mio_to_write_with.write(mreject.into_bytes().as_slice());
-                    }
-                },
-
-
-            	Join => {
-                    let player_id = cm.sender().clone();
-                    let mid = cm.id().clone();
-
-                    if self.verify_id(&player_id, tok) {
-                        match self.userid_to_room.entry(player_id.clone()) {
-                            //already part of a room, ask them to leave it
-                            Occupied(_) => {
-                                let mreject = Message::new(cm.id().clone(), UTC::now(),
-                                            "SERVER".to_string(), cm.sender().clone(), Reject(mid), "Please leave your room first".to_string());
-                                self.handle_to_mio_to_write_with.write(mreject.into_bytes().as_slice());
-                            },
-
-                            //user is free to join any room they wnat
-                            Vacant(good_entry) => {
-                                // room exists - go for it
-                                if self.id_to_room.contains_key(cm.message()) {
-                                    good_entry.insert(cm.message().clone());
-                                    let mconfirm = Message::new(cm.id().clone(), UTC::now(),
-                                                "SERVER".to_string(), cm.sender().clone(), Confirm(mid), format!("Welcome to: {}", cm.message()));
-                                    self.handle_to_mio_to_write_with.write(mconfirm.into_bytes().as_slice());
-                                } else {
-                                    let mreject = Message::new(cm.id().clone(), UTC::now(),
-                                                "SERVER".to_string(), cm.sender().clone(), Reject(mid), "No room with that name found".to_string());
-                                    self.handle_to_mio_to_write_with.write(mreject.into_bytes().as_slice());
-                                }
-                            }
-                        }
-                    }
-                }
-
-            	Leave => {
-                    let player_id = cm.sender().clone();
-                    let mid = cm.id().clone();
-
-                    if self.verify_id(&player_id, tok) {
-                        match self.id_to_room.entry(player_id) {
-                            //already part of a room, ask them to leave it
-                            Vacant(_) => {
-                                let mreject = Message::new(cm.id().clone(), UTC::now(), "SERVER".to_string(),
-                                    cm.sender().clone(), Reject(mid), "You are not currently in a room".to_string());
-                                self.handle_to_mio_to_write_with.write(mreject.into_bytes().as_slice());
-                            },
-
-                            //user is in a room, make it blank
-                            Occupied(good_entry) => {
-                                good_entry.remove();
-                                let mconfirm = Message::new(cm.id().clone(), UTC::now(),
-                                            "SERVER".to_string(), cm.sender().clone(), Confirm(mid), "You've left the room.".to_string());
-                                self.handle_to_mio_to_write_with.write(mconfirm.into_bytes().as_slice());
-                            }
-                        }
-                    }
-                },
-
-            	Action => {
-                    unimplemented!();
-                },
+            	Action => { self.handle_action(cm, tok); },
 
                 _ => {
                     //It's either a confirm or reject
@@ -139,6 +57,97 @@ impl<T : Write> ChatApp<T> {
         } else {
             unimplemented!();
         }
+    }
+
+    fn handle_connect(&mut self, cm : Message, tok : Token) {
+        let player_id = cm.message().clone();
+        let mid = cm.id().clone();
+
+        if let Vacant(ic_entry) = self.id_to_conn.entry(player_id.clone()) {
+            //new connection - new name
+            if let Vacant(ci_entry) = self.conn_to_id.entry(tok) {
+                ic_entry.insert(tok);
+                ci_entry.insert(player_id);
+
+                let mconfirm = Message::new(cm.id().clone(), UTC::now(),
+                            "SERVER".to_string(), cm.sender().clone(), Confirm(mid), "".to_string());
+                self.handle_to_mio_to_write_with.write(mconfirm.into_bytes().as_slice());
+            }
+
+            //old connection wants new name
+            else {
+                //could handle this more elegantly, but very unlikely unless spoofing packets
+                unreachable!();
+            }
+        }
+
+        // id already taken, refuse "connection"
+        else {
+            let mreject = Message::new(cm.id().clone(), UTC::now(),
+                        "SERVER".to_string(), cm.sender().clone(), Reject(mid), "Requested ID is already taken".to_string());
+            self.handle_to_mio_to_write_with.write(mreject.into_bytes().as_slice());
+        }
+    }
+
+    fn handle_join(&mut self, cm : Message, tok : Token) {
+        let player_id = cm.sender().clone();
+        let mid = cm.id().clone();
+
+        if self.verify_id(&player_id, tok) {
+            match self.userid_to_room.entry(player_id.clone()) {
+                //already part of a room, ask them to leave it
+                Occupied(_) => {
+                    let mreject = Message::new(cm.id().clone(), UTC::now(),
+                                "SERVER".to_string(), cm.sender().clone(), Reject(mid), "Please leave your room first".to_string());
+                    self.handle_to_mio_to_write_with.write(mreject.into_bytes().as_slice());
+                },
+
+                //user is free to join any room they wnat
+                Vacant(good_entry) => {
+                    // room exists - go for it
+                    if self.id_to_room.contains_key(cm.message()) {
+                        good_entry.insert(cm.message().clone());
+                        let mconfirm = Message::new(cm.id().clone(), UTC::now(),
+                                    "SERVER".to_string(), cm.sender().clone(), Confirm(mid), format!("Welcome to: {}", cm.message()));
+                        self.handle_to_mio_to_write_with.write(mconfirm.into_bytes().as_slice());
+                    } else {
+                        let mreject = Message::new(cm.id().clone(), UTC::now(),
+                                    "SERVER".to_string(), cm.sender().clone(), Reject(mid), "No room with that name found".to_string());
+                        self.handle_to_mio_to_write_with.write(mreject.into_bytes().as_slice());
+                    }
+                }
+            }
+        }
+    }
+
+    fn handle_leave(&mut self, cm : Message, tok : Token) {
+        let player_id = cm.sender().clone();
+        let mid = cm.id().clone();
+
+        if self.verify_id(&player_id, tok) {
+            match self.id_to_room.entry(player_id) {
+                //not in a room to leave from
+                Vacant(_) => {
+                    let mreject = Message::new(cm.id().clone(), UTC::now(), "SERVER".to_string(),
+                        cm.sender().clone(), Reject(mid), "You are not currently in a room".to_string());
+                    self.handle_to_mio_to_write_with.write(mreject.into_bytes().as_slice());
+                },
+
+                //user is in a room, make it blank
+                Occupied(good_entry) => {
+                    good_entry.remove();
+                    let mconfirm = Message::new(cm.id().clone(), UTC::now(),
+                                "SERVER".to_string(), cm.sender().clone(), Confirm(mid), "You've left the room.".to_string());
+                    self.handle_to_mio_to_write_with.write(mconfirm.into_bytes().as_slice());
+                }
+            }
+        }
+    }
+
+
+
+    fn handle_action(&mut self, cm : Message, tok : Token) {
+        unimplemented!()
     }
 
 
